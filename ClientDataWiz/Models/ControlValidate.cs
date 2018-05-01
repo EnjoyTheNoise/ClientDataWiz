9f69a9258c4c2400c51a8655b159203acfa2427f
EnjoyTheNoise <jankos.pdp@gmail.com>
2018-05-01 14:14:12 +0200

    Simple MainWindow, add control validation model

---
+++ ClientDataWiz/Models/ControlValidate.cs
@@ -0,0 +1,10 @@
+﻿namespace ClientDataWiz.Models
+{
+    public class ControlValidate
+    {
+        public bool FirstName { get; set; }
+        public bool LastName { get; set; }
+        public bool Address { get; set; }
+        public bool Phone { get; set; }
+    }
+}
