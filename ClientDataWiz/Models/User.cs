22eafa524b8c7b6197d294d713164b28e41bfdaf
EnjoyTheNoise <jankos.pdp@gmail.com>
2018-05-01 13:45:27 +0200

    add User model, add BaseViewModel (INotifyPropertyChanged implementation)

---
+++ ClientDataWiz/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace ClientDataWiz.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ ClientDataWiz/ViewModels/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace ClientDataWiz.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public static event PropertyChangedEventHandler StaticPropertyChanged;
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, e);
+        }
+
+        protected void OnPropertyChanged(string propName)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propName));
+        }
+
+        public static void OnStaticPropertyChanged(string propName)
+        {
+            StaticPropertyChanged?.Invoke(null, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
